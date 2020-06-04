f2064911455895915ba14238080d06bdd4b9c20c
Ivan Baena <baenadev.io@gmail.com>
2020-06-04 00:28:04 -0400

    timer complete

--- index.js
+++ index.js
@@ -1,62 +1,21 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    if (callbacks) {
-      this.onStart = callbacks.onStart;
-      this.onTick = callbacks.onTick;
-      this.onComplete = callbacks.onComplete;
-    }
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    if (this.onStart) {
-      this.onStart();
-    }
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-      if (this.onComplete) {
-        this.onComplete();
-      }
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-      if (this.onTick) this.onTick();
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-
-  pause = () => {
-    const { interval } = this;
-    clearInterval(interval);
-  };
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log('Timer started');
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log('timer is ticking');
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      'stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
   },
   onComplete() {
     console.log('timer completed');
